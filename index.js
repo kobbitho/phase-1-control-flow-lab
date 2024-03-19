c82e50dfb3ee347ef1aa22eff733c08accc2d7a8
Tabithaokoth <kobbitho@gmail.com>
2024-03-19 21:36:17 +0300

    adds

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <=400){
+    return 'This one is on me!'
+  }else if (distance >= 400 && distance <= 2000){
+    return 'That will be twenty bucks.'
+  }else if (distance >2000 && distance <=2500){
+    return 'I will gladly take your thirty bucks.'
+  }else if (distance >2500){
+    return 'No can do.'
+  }
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+return (city ==='NYC')? 'Ok, sounds good.' :'No go.';
+
+  console.log(ternaryCheckCity('NYC'));
+  
+
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(generosityLevel){
   // Write your code here!
+  let response = 'Bye'
+  switch (generosityLevel) {
+    case 'generous':
+      response = 'Thank you so much.'
+      break;
+    case 'not as generous':
+      response = 'Thank you.'
+      break;
+      case 'If anything else':
+        response = 'Bye.'
+        break;
+        default:
+          response = 'Bye.';
+  }
+  return response;
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+console.log(switchOnCharmFromTip('generous'));
